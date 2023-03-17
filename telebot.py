abc93e59809b010d31c290d2ace5edd57ab5206b
Dwayne Redwood <87104481+coderedwood@users.noreply.github.com>
2023-03-17 04:28:53 -0500

    initial commit

---
+++ telebot.py
@@ -0,0 +1,66 @@
+import os
+from dotenv import load_dotenv
+from telegram import *
+from telegram.ext import *
+
+load_dotenv()
+
+print("Starting Bot")
+apitoken = os.getenv("API_KEY")
+
+# Define a function to handle incoming messages
+
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE)-> None:
+    await update.message.reply_text("Hello there! How goes thee?")
+
+async def help_command(update, context: ContextTypes.DEFAULT_TYPE)-> None:
+    await update.message.reply_text("Help message")
+
+async def custom_command(update, context: ContextTypes.DEFAULT_TYPE)->None:
+    await update.message.reply_text("Hello there! How goes thee?")
+
+def handle_response(text: str) -> str:
+    if 'hello' in text:
+        return 'Hey there'
+    if 'how are you' in text:
+        return "I am good, thanks!"
+
+    return 'idk'
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE)-> None:
+    message_type = update.message.chat.type
+    text = str(update.message.text).lower()
+    response = ''
+
+    if message_type == 'group':
+        if '@MT5Signalsbot' in text:
+            new_text = text.replace('MT5Signalsbot', '').strip()
+            response = handle_response(new_text)
+    else:
+        response = handle_response(text)
+
+    await update.message.reply_text(response)
+
+
+def error(update: Update, context: ContextTypes.DEFAULT_TYPE)-> None:
+    print(f'Update {update} caused error:n{context.error}')
+
+if __name__ == '__main__':
+    # updater = Updater(apitoken, queue.Queue())
+    dp = Application.builder().token(apitoken).build()
+
+
+    # Commands
+    dp.add_handler(CommandHandler('start', start_command))
+    dp.add_handler(CommandHandler('help', help_command))
+    dp.add_handler(CommandHandler('custom', custom_command))
+
+    # Messages
+    dp.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+    # Errors
+    dp.add_error_handler(error)
+
+    # Run bot
+    dp.run_polling()
